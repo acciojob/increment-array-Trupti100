012e4a143ecedf08bb2a174100d0de9191183b84
Trupti100 <127935389+Trupti100@users.noreply.github.com>
2023-09-08 09:37:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-let arr = [1, 2, 3, 4, 5];
 function incrementArray(arr) {
-  //your code here
+  // Create a new array to store the incremented values
+  const result = [];
+
+  // Loop through the input array and increment each element by 1
+  for (let i = 0; i < arr.length; i++) {
+    result.push(arr[i] + 1);
+  }
+
+  return result;
 }
 
-alert(incrementArray(arr));
+// Test cases
+console.log(incrementArray([1, 2, 3, 4, 5])); // Output: [2, 3, 4, 5, 6]
+console.log(incrementArray([0, 0, 0])); // Output: [1, 1, 1]
+console.log(incrementArray([-2, -1, 0])); // Output: [-1, 0, 1]
+
